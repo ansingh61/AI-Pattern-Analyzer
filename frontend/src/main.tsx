a93b12c4550eed06769d6543e3d15b505f4f9e98
ansingh61 <ansingh61@deloitte.com>
2026-09-08 20:23:34 +0530

    reposiveness update

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './app/App';
 import './styles/globals.css';
 import './styles/theme.css';
+import './styles/responsive.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
